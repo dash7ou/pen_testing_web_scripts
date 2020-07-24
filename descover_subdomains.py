b48cd6a0af5832fdab9af4eb360b52949af338dc
dashzou <fxfddfno@gmail.com>
2020-07-24 04:16:33 -0400

    discover all sub domains

--- descover_subdomains.py
+++ descover_subdomains.py
@@ -4,6 +4,18 @@ import requests
 
 def make_request(url):
     try:
-        return response.get("http://" + url)
+        return requests.get("http://" + url)
     except requests.exceptions.ConnectionError:
         pass
+
+
+def discover_subdomains(url):
+    with open("./subdomainslist.txt", "r") as words_list:
+        for line in words_list:
+            test_url = line.strip() + "." + url
+            response = make_request(test_url)
+            if response:
+                print("[+] Discovered Subdomain ===> ", test_url)
+
+
+discover_subdomains("google.com")
